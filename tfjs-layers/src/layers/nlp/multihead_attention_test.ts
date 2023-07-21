bc73a0fda9c179ea88c951005d871895fd644bcd
Piero Orderique <porderiq@mit.edu>
2023-07-21 12:13:55 -0700

    Add masked attention bias initial tests

--- tfjs-layers/src/layers/nlp/multihead_attention_test.ts
+++ tfjs-layers/src/layers/nlp/multihead_attention_test.ts
@@ -19,7 +19,7 @@
  * Unit Tests for MultiHeadAttention layer.
  */
 
-import { Tensor, ones } from '@tensorflow/tfjs-core';
+import { Tensor, ones, tensor } from '@tensorflow/tfjs-core';
 
 import { Shape } from '../../keras_format/common';
 import { MultiHeadAttention } from './multihead_attention';
@@ -27,8 +27,9 @@ import { SymbolicTensor } from '../../engine/topology';
 import { input, model } from '../../exports';
 
 describe('MultiHeadAttention', () => {
+  interface TestArgs {};
 
-  interface NonMaskedAttentionArgs {
+  interface NonMaskedAttentionArgs extends TestArgs {
     testcaseName: string,
     valueDim: number,
     outputShape: Shape,
@@ -55,7 +56,7 @@ describe('MultiHeadAttention', () => {
     });
   }
 
-  let params: NonMaskedAttentionArgs[] = [
+  let params: TestArgs[] = [
     {
       testcaseName: 'key value same proj',
       valueDim: null,
@@ -70,7 +71,7 @@ describe('MultiHeadAttention', () => {
     }
   ];
   for (const param of params) {
-    testNonMaskedAttention(param);
+    testNonMaskedAttention(param as NonMaskedAttentionArgs);
   }
 
   // Test with one input (self-attenntion) and no mask tensor.
@@ -103,7 +104,7 @@ describe('MultiHeadAttention', () => {
     expect(coef.shape).toEqual([1, 12, 40, 60]);
   });
 
-  interface MaskedAttentionArgs {
+  interface MaskedAttentionArgs extends TestArgs {
     testcaseName: string,
     useBias: boolean,
   };
@@ -124,12 +125,79 @@ describe('MultiHeadAttention', () => {
       const attentionMask = input({shape: [4, 2]});
       const output =
         testLayer.apply(query, {value, attentionMask}) as SymbolicTensor;
-      // const output = input({shape: [4, 8]});
+
       // Create a model containing the test layer.
       const mha =
         model({inputs: [query, value, attentionMask], outputs: output});
-      // ! Left off here. Perhaps do this test case later? const modes = model()
+
+      function getRandomSample(shape: Shape, max: number): Tensor {
+        const randomSample: number[][][] = [];
+        for (let i = 0; i < shape[0]; i++) {
+          const subArray: number[][] = [];
+          for (let j = 0; j < shape[1]; j++) {
+            const subSubArray: number[] = [];
+            for (let k = 0; k < shape[2]; k++) {
+              subSubArray.push(Math.random() * max);
+            }
+            subArray.push(subSubArray);
+          }
+          randomSample.push(subArray);
+        }
+        return tensor(randomSample);
+      }
+
+      function getRandomInt(max: number): number {
+        return Math.floor(Math.random() * Math.floor(max));
+      }
+
+      function getRandomMaskData(shape: Shape): Tensor {
+        const maskData: number[][][] = [];
+        for (let i = 0; i < shape[0]; i++) {
+          const subArray: number[][] = [];
+          for (let j = 0; j < shape[1]; j++) {
+            const subSubArray: number[] = [];
+            for (let k = 0; k < shape[2]; k++) {
+              subSubArray.push(getRandomInt(2));
+            }
+            subArray.push(subSubArray);
+          }
+          maskData.push(subArray);
+        }
+        return tensor(maskData);
+      }
+
+      // Generate data for the input (non-mask) tensors.
+      const fromData = getRandomSample([batchSize, 4, 8], 10);
+      const toData = getRandomSample([batchSize, 2, 8], 10);
+
+      // Invoke the data with a random set of mask data. This should mask at
+      // least one element.
+      const maskData = getRandomMaskData([batchSize, 4, 2]);
+      const maskedOutputData =
+        mha.predict([fromData, toData, maskData]) as Tensor;
+
+      // Invoke the same data, but with a null mask (where no elements are
+      // masked).
+      const nullMaskData = ones([batchSize, 4, 2]);
+      const unmaskedOutputData =
+        mha.predict([fromData, toData, nullMaskData]) as Tensor;
+
+      expect(maskedOutputData.dataSync()).not.toEqual(
+        unmaskedOutputData.dataSync());
     });
+  }
+  params = [
+    {
+      testcaseName: 'with bias',
+      useBias: true,
+    },
+    {
+      testcaseName: 'no bias',
+      useBias: false,
+    }
+  ];
+  for (const param of params) {
+    testMaskedAttention(param as MaskedAttentionArgs);
   }
   // TODO(pforderique): Test memory and serialization.
 });
