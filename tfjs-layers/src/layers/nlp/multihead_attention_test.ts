11bcff996fcbe5546fc8c84901ff6ea248463ab6
Piero Orderique <porderiq@mit.edu>
2023-07-24 14:05:32 -0700

    Add Masked Attention test

--- tfjs-layers/src/layers/nlp/multihead_attention_test.ts
+++ tfjs-layers/src/layers/nlp/multihead_attention_test.ts
@@ -28,10 +28,9 @@ import { MultiHeadAttention } from './multihead_attention';
 import { describeMathCPU, expectTensorsNotClose } from '../../utils/test_utils';
 
 describe('MultiHeadAttention', () => {
-  interface TestArgs {};
 
   describe('Non Masked Attention', () => {
-    interface NonMaskedAttentionArgs extends TestArgs {
+    interface NonMaskedAttentionArgs {
       testcaseName: string;
       valueDim: number;
       outputShape: Shape;
@@ -58,7 +57,7 @@ describe('MultiHeadAttention', () => {
       });
     }
 
-    const params: TestArgs[] = [
+    const params: NonMaskedAttentionArgs[] = [
       {
         testcaseName: 'key value same proj',
         valueDim: null,
@@ -73,7 +72,7 @@ describe('MultiHeadAttention', () => {
       }
     ];
     for (const param of params) {
-      testNonMaskedAttention(param as NonMaskedAttentionArgs);
+      testNonMaskedAttention(param);
     }
   });
 
@@ -107,101 +106,65 @@ describe('MultiHeadAttention', () => {
     expect(coef.shape).toEqual([1, 12, 40, 60]);
   });
 
-  // interface MaskedAttentionArgs extends TestArgs {
-  //   testcaseName: string;
-  //   useBias: boolean;
-  // };
-  // /**
-  //  * Test with a mask tensor.
-  //  */
-  // function testMaskedAttention({testcaseName, useBias}: MaskedAttentionArgs) {
-  //   it(`${testcaseName} masked attention`, () => {
-  //     const testLayer = new MultiHeadAttention({
-  //       numHeads: 2,
-  //       keyDim: 2,
-  //       useBias,
-  //     });
-  //     // Create a 3-dimensional input (the first dimension is implicit).
-  //     const batchSize = 3;
-  //     const query = input({shape: [4, 8]});
-  //     const value = input({shape: [2, 8]});
-  //     const attentionMask = input({shape: [4, 2]});
-  //     const output =
-  //       testLayer.apply(query, {value, attentionMask}) as SymbolicTensor;
+  // !Left off here! Ready to translate this code -->
+  // !maybe also go ahead and make queryDense private? and use getter?
+  describe('Masked Attention', () => {
+    interface MaskedAttentionArgs {
+      testcaseName: string;
+      useBias: boolean;
+    };
+    /**
+     * Test with a mask tensor.
+     */
+    function testMaskedAttention({testcaseName, useBias}: MaskedAttentionArgs) {
+      it(`${testcaseName} masked attention`, () => {
+        const testLayer = new MultiHeadAttention({
+          numHeads: 2,
+          keyDim: 2,
+          useBias,
+        });
+        // Create a 3-dimensional input (the first dimension is implicit).
+        const batchSize = 3;
+        const query = randomUniform([4, 8]);
+        const value = randomUniform([2, 8]);
 
-  //     // Create a model containing the test layer.
-  //     const mha =
-  //       model({inputs: [query, value, attentionMask], outputs: output});
+        // Invoke the data with a random set of mask data. This should mask at
+        // least one element.
+        const maskData = randomUniformInt([batchSize, 4, 2], 0, 2);
+        const maskedOutputData = testLayer.call(
+          query, {value, attentionMask: maskData});
 
-  //     function getRandomSample(shape: Shape, max: number): Tensor {
-  //       const randomSample: number[][][] = [];
-  //       for (let i = 0; i < shape[0]; i++) {
-  //         const subArray: number[][] = [];
-  //         for (let j = 0; j < shape[1]; j++) {
-  //           const subSubArray: number[] = [];
-  //           for (let k = 0; k < shape[2]; k++) {
-  //             subSubArray.push(Math.random() * max);
-  //           }
-  //           subArray.push(subSubArray);
-  //         }
-  //         randomSample.push(subArray);
-  //       }
-  //       return tensor(randomSample);
-  //     }
+        // Invoke the same data, but with a null mask (where no elements are
+        // masked).
+        const nullMaskData = ones([batchSize, 4, 2]);
+        const unmaskedOutputData = testLayer.call(
+          query, {value, attentionMask: nullMaskData});
 
-  //     function getRandomInt(max: number): number {
-  //       return Math.floor(Math.random() * Math.floor(max));
-  //     }
+        expectTensorsNotClose(maskedOutputData, unmaskedOutputData);
 
-  //     function getRandomMaskData(shape: Shape): Tensor {
-  //       const maskData: number[][][] = [];
-  //       for (let i = 0; i < shape[0]; i++) {
-  //         const subArray: number[][] = [];
-  //         for (let j = 0; j < shape[1]; j++) {
-  //           const subSubArray: number[] = [];
-  //           for (let k = 0; k < shape[2]; k++) {
-  //             subSubArray.push(getRandomInt(2));
-  //           }
-  //           subArray.push(subSubArray);
-  //         }
-  //         maskData.push(subArray);
-  //       }
-  //       return tensor(maskData);
-  //     }
-
-  //     // Generate data for the input (non-mask) tensors.
-  //     const fromData = getRandomSample([batchSize, 4, 8], 10);
-  //     const toData = getRandomSample([batchSize, 2, 8], 10);
-
-  //     // Invoke the data with a random set of mask data. This should mask at
-  //     // least one element.
-  //     const maskData = getRandomMaskData([batchSize, 4, 2]);
-  //     const maskedOutputData =
-  //       mha.predict([fromData, toData, maskData]) as Tensor;
-
-  //     // Invoke the same data, but with a null mask (where no elements are
-  //     // masked).
-  //     const nullMaskData = ones([batchSize, 4, 2]);
-  //     const unmaskedOutputData =
-  //       mha.predict([fromData, toData, nullMaskData]) as Tensor;
-
-  //     expect(maskedOutputData.dataSync()).not.toEqual(
-  //       unmaskedOutputData.dataSync());
-  //   });
-  // }
-  // params = [
-  //   {
-  //     testcaseName: 'with bias',
-  //     useBias: true,
-  //   },
-  //   {
-  //     testcaseName: 'no bias',
-  //     useBias: false,
-  //   }
-  // ];
-  // for (const param of params) {
-  //   testMaskedAttention(param as MaskedAttentionArgs);
-  // }
+        if (useBias) {
+          expect(testLayer.queryDense.trainableWeights.length).toEqual(2);
+          expect(testLayer.outputDense.trainableWeights.length).toEqual(2);
+        } else {
+          expect(testLayer.queryDense.trainableWeights.length).toEqual(1);
+          expect(testLayer.outputDense.trainableWeights.length).toEqual(1);
+        }
+      });
+    }
+    const params: MaskedAttentionArgs[] = [
+      {
+        testcaseName: 'with bias',
+        useBias: true,
+      },
+      {
+        testcaseName: 'no bias',
+        useBias: false,
+      }
+    ];
+    for (const param of params) {
+      testMaskedAttention(param);
+    }
+  });
 
   // Test with a specified initializer
   it('initializers', () => {
@@ -232,7 +195,7 @@ describe('MultiHeadAttention', () => {
   // TODO(pforderique): Change to regular `describe` once GPU supports math for
   // rank 7 tensors is supported.
   describeMathCPU('High Dimensional Attention', () => {
-    interface HighDimAttentionArgs extends TestArgs {
+    interface HighDimAttentionArgs {
       testcaseName: string;
       qDims: Shape;
       vDims: Shape;
@@ -277,7 +240,7 @@ describe('MultiHeadAttention', () => {
         expectTensorsNotClose(outputWithMask, outputWithNullMask);
       });
     }
-    const params = [
+    const params: HighDimAttentionArgs[] = [
       {
         testcaseName: '4d_inputs_1freebatch_mask2',
         qDims: [3, 4],
@@ -322,8 +285,7 @@ describe('MultiHeadAttention', () => {
       },
     ];
     for (const param of params) {
-      // testHighDimAttention; param;
-      testHighDimAttention(param as HighDimAttentionArgs);
+      testHighDimAttention(param);
     }
   });
   // TODO(pforderique): Test memory and serialization.
