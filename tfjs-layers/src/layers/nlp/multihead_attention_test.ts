a34e98db8ceef0e14d07b68b53d115a8c797dbe3
Piero Orderique <porderiq@mit.edu>
2023-07-24 12:40:39 -0700

    Wrap whole test

--- tfjs-layers/src/layers/nlp/multihead_attention_test.ts
+++ tfjs-layers/src/layers/nlp/multihead_attention_test.ts
@@ -227,103 +227,103 @@ describe('MultiHeadAttention', () => {
     // expectTensorsNotClose(queryKernel, outputKernel);
   });
 
-  interface HighDimAttentionArgs extends TestArgs {
-    testcaseName: string;
-    qDims: Shape;
-    vDims: Shape;
-    maskDims: Shape;
-    attentionAxes: number[];
-  };
-  /**
-   * Test with high dimensional inputs.
-   */
-  function testHighDimAttention({
-    testcaseName, qDims, vDims, maskDims, attentionAxes,
-  }: HighDimAttentionArgs) {
-    describeMathCPU('High dimensions', () => {
-    it(`${testcaseName} high dim attention`, () => {
-      const testLayer = new MultiHeadAttention({
-        numHeads: 2, keyDim: 2, attentionAxes,
+  describeMathCPU('High dimensions', () => {
+    interface HighDimAttentionArgs extends TestArgs {
+      testcaseName: string;
+      qDims: Shape;
+      vDims: Shape;
+      maskDims: Shape;
+      attentionAxes: number[];
+    };
+    /**
+     * Test with high dimensional inputs.
+     */
+    function testHighDimAttention({
+      testcaseName, qDims, vDims, maskDims, attentionAxes,
+    }: HighDimAttentionArgs) {
+      it(`${testcaseName} high dim attention`, () => {
+        const testLayer = new MultiHeadAttention({
+          numHeads: 2, keyDim: 2, attentionAxes,
+        });
+        const batchSize = 3;
+        const hiddenSize = 8;
+        // Generate data for the input (non-mask) tensors.
+        const queryShape = [batchSize].concat(qDims).concat(hiddenSize);
+        const valueShape = [batchSize].concat(vDims).concat(hiddenSize);
+        const maskShape = [batchSize].concat(maskDims);
+        const query = randomUniform(queryShape, 0, 10);
+        const value = randomUniform(valueShape, 0, 10);
+
+        // Invoke the data with a random set of mask data. This should mask at
+        // least one element.
+        const maskData = randomUniformInt(maskShape, 0, 2).asType('bool');
+
+        // Invoke the same data, but with a null mask (where no elements are
+        // masked).
+        const nullMaskData = ones(maskShape);
+
+        // Because one data is masked and one is not, the outputs should not be
+        // the same.
+
+        const outputWithMask = testLayer.call(
+          query, {value, attentionMask: maskData});
+        const outputWithNullMask = testLayer.call(
+          query, {value, attentionMask: nullMaskData});
+
+        expectTensorsNotClose(outputWithMask, outputWithNullMask);
       });
-      const batchSize = 3;
-      const hiddenSize = 8;
-      // Generate data for the input (non-mask) tensors.
-      const queryShape = [batchSize].concat(qDims).concat(hiddenSize);
-      const valueShape = [batchSize].concat(vDims).concat(hiddenSize);
-      const maskShape = [batchSize].concat(maskDims);
-      const query = randomUniform(queryShape, 0, 10);
-      const value = randomUniform(valueShape, 0, 10);
-
-      // Invoke the data with a random set of mask data. This should mask at
-      // least one element.
-      const maskData = randomUniformInt(maskShape, 0, 2).asType('bool');
-
-      // Invoke the same data, but with a null mask (where no elements are
-      // masked).
-      const nullMaskData = ones(maskShape);
-
-      // Because one data is masked and one is not, the outputs should not be
-      // the same.
-
-      const outputWithMask = testLayer.call(
-        query, {value, attentionMask: maskData});
-      const outputWithNullMask = testLayer.call(
-        query, {value, attentionMask: nullMaskData});
-
-      expectTensorsNotClose(outputWithMask, outputWithNullMask);
-    });
-    });
-  }
-  params = [
-    {
-      testcaseName: '4d_inputs_1freebatch_mask2',
-      qDims: [3, 4],
-      vDims: [3, 2],
-      maskDims: [4, 2],
-      attentionAxes: [2],
-    },
-    {
-      testcaseName: '4d_inputs_1freebatch_mask3',
-      qDims: [3, 4],
-      vDims: [3, 2],
-      maskDims: [3, 4, 2],
-      attentionAxes: [2],
-    },
-    {
-      testcaseName: '4d_inputs_1freebatch_mask4',
-      qDims: [3, 4],
-      vDims: [3, 2],
-      maskDims: [3, 2, 4, 2],
-      attentionAxes: [2],
-    },
-    // TODO(pforderique): Add test cases '4D_inputs_2D_attention',
-    // '5D_inputs_2D_attention', and '5D_inputs_2D_attention_fullmask' once
-    // GPU for rank 7 tensors is supported.
-    {
-      testcaseName: '4D_inputs_2D_attention',
-      qDims: [3, 4],
-      vDims: [3, 2],
-      maskDims: [3, 4, 3, 2],
-      attentionAxes: [1, 2],
-    },
-    {
-      testcaseName: '5D_inputs_2D_attention',
-      qDims: [5, 3, 4],
-      vDims: [5, 3, 2],
-      maskDims: [3, 4, 3, 2],
-      attentionAxes: [2, 3],
-    },
-    {
-      testcaseName: '5D_inputs_2D_attention_fullmask',
-      qDims: [5, 3, 4],
-      vDims: [5, 3, 2],
-      maskDims: [5, 3, 4, 3, 2],
-      attentionAxes: [2, 3],
-    },
-  ];
-  for (const param of params) {
-    // testHighDimAttention; param;
-    testHighDimAttention(param as HighDimAttentionArgs);
-  }
+    }
+    params = [
+      {
+        testcaseName: '4d_inputs_1freebatch_mask2',
+        qDims: [3, 4],
+        vDims: [3, 2],
+        maskDims: [4, 2],
+        attentionAxes: [2],
+      },
+      {
+        testcaseName: '4d_inputs_1freebatch_mask3',
+        qDims: [3, 4],
+        vDims: [3, 2],
+        maskDims: [3, 4, 2],
+        attentionAxes: [2],
+      },
+      {
+        testcaseName: '4d_inputs_1freebatch_mask4',
+        qDims: [3, 4],
+        vDims: [3, 2],
+        maskDims: [3, 2, 4, 2],
+        attentionAxes: [2],
+      },
+      // TODO(pforderique): Add test cases '4D_inputs_2D_attention',
+      // '5D_inputs_2D_attention', and '5D_inputs_2D_attention_fullmask' once
+      // GPU for rank 7 tensors is supported.
+      {
+        testcaseName: '4D_inputs_2D_attention',
+        qDims: [3, 4],
+        vDims: [3, 2],
+        maskDims: [3, 4, 3, 2],
+        attentionAxes: [1, 2],
+      },
+      {
+        testcaseName: '5D_inputs_2D_attention',
+        qDims: [5, 3, 4],
+        vDims: [5, 3, 2],
+        maskDims: [3, 4, 3, 2],
+        attentionAxes: [2, 3],
+      },
+      {
+        testcaseName: '5D_inputs_2D_attention_fullmask',
+        qDims: [5, 3, 4],
+        vDims: [5, 3, 2],
+        maskDims: [5, 3, 4, 3, 2],
+        attentionAxes: [2, 3],
+      },
+    ];
+    for (const param of params) {
+      // testHighDimAttention; param;
+      testHighDimAttention(param as HighDimAttentionArgs);
+    }
+  });
   // TODO(pforderique): Test memory and serialization.
 });
